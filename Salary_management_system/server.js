bd9c6f209950f97600accbcdf9053d1e6974c74c
Gonghb <1085242202@qq.com>
2017-04-08 00:31:44 +0800

    update server.js

--- Salary_management_system/server.js
+++ Salary_management_system/server.js
@@ -40,7 +40,7 @@ app.use(session({
     saveUninitialized: true,
 }));
 
-var con = new mssql.Connection(config, function (err) {  
+var con = new mssql.Connection(config,(err) => {  
       if (err) {  
         console.log(err);  
         return;  
@@ -50,15 +50,15 @@ var con = new mssql.Connection(config, function (err) {
 
 
 //index
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
     res.render('pages/login');
 });
 
-app.post('/',function(req,res){
-    var usernm = req.body.username;
-    var passwd = req.body.password;
-    var request = new mssql.Request(con);
-    request.query("select * from usr where username='"+usernm+"' and password='"+passwd+"'", function(err, recordset){
+app.post('/',(req,res) => {
+    let usernm = req.body.username;
+    let passwd = req.body.password;
+    let request = new mssql.Request(con);
+    request.query("select * from usr where username='"+usernm+"' and password='"+passwd+"'", (err, recordset) => {
       if(err){
         res.redirect('/');
       }else{
@@ -72,7 +72,7 @@ app.post('/',function(req,res){
     });
 });
 
-app.get('/index', function(req, res){
+app.get('/index', (req, res) => {
     if(!req.session.user){
       res.redirect('/');
       // res.render('pages/index');
@@ -81,23 +81,23 @@ app.get('/index', function(req, res){
     }
 });
 
-app.get('/logout', function(req, res){
+app.get('/logout', (req, res) => {
     req.session.user = null;
     res.redirect('/');
 });
 
-app.post('/delete', function(req, res){
-  var id = req.body.id;
-  var i = req.body.type;
-  var table = new Array("base_information","wage","wage");
-  var request = new mssql.Request(con);
+app.post('/delete', (req, res) => {
+  let id = req.body.id;
+  let i = req.body.type;
+  let table = ["base_information","wage","wage"];
+  let request = new mssql.Request(con);
   if(i == 1){
-    request.query("delete from wage where e_id = "+id, function(err, recordset){
+    request.query("delete from wage where e_id = "+id, (err, recordset) => {
     });
-    request.query("update base_information set superior_id ="+null+" where superior_id = "+id, function(err, recordset){
+    request.query("update base_information set superior_id ="+null+" where superior_id = "+id, (err, recordset) => {
     });
   }
-  request.query("delete from "+table[i-1]+" where id = "+id, function(err, recordset){
+  request.query("delete from "+table[i-1]+" where id = "+id, (err, recordset) => {
     if(err){
       res.send('error');
     }else{
@@ -105,35 +105,44 @@ app.post('/delete', function(req, res){
     }
   });
 });
-app.post('/search', function(req, res){
-  var i = req.body.type;
-  var content = req.body.content;
-  var x = req.body.x;
-  var table = new Array("base_information","wage","wage");
-  var type = new Array("id","name","superior_id","department_id","e_id","d_id","year","month");
-  var request = new mssql.Request(con);
+app.post('/search', (req, res) => {
+
+  let i = req.body.type;
+  let content = req.body.content;
+  let x = req.body.x;
+  let table = ["base_information","wage","wage"];
+  let type = ["id","name","superior_id","department_id","e_id","d_id","year","month"];
+  let request = new mssql.Request(con);
   if( i == 2){
-    var sql = "select * from "+table[x-1]+" where "+type[i-1]+" like '%"+content+"%' order by e_id,year,month";
+    if(x == 1)
+      var sql = "select * from "+table[x-1]+" where "+type[i-1]+" like '%"+content+"%'";
+    else
+      var sql = "select * from "+table[x-1]+" where "+type[i-1]+" like '%"+content+"%' order by e_id,year,month";
   }else{
-    var sql = "select * from "+table[x-1]+" where "+type[i-1]+" = "+content+" order by e_id,year,month";
+    if(x == 1)
+      var sql = "select * from "+table[x-1]+" where "+type[i-1]+" = "+content+"";
+    else
+      var sql = "select * from "+table[x-1]+" where "+type[i-1]+" = "+content+" order by e_id,year,month";
+
   }
-  request.query(sql, function(err, recordset){
+  request.query(sql, (err, recordset) => {
     if(err){
+      // console.log(err)
       res.send('error');
     }else{
       res.send(recordset);
     }
   });
 });
-app.post('/save', function(req, res){
-  var inf = req.body;
-  var request = new mssql.Request(con);
-  var sql = new Array("update base_information set name ='"+inf.name+"', superior_id=isnull(NULL,'"+inf.superior_id+"'), department_id=isnull(NULL,'"+inf.department_id+"'), base_wage=isnull(NULL,'"+inf.base_wage+"'), House_wage=isnull(NULL,'"+inf.House_wage+"') where id = "+inf.id,
+app.post('/save', (req, res) => {
+  let inf = req.body;
+  let request = new mssql.Request(con);
+  let sql = ["update base_information set name ='"+inf.name+"', superior_id=isnull(NULL,'"+inf.superior_id+"'), department_id=isnull(NULL,'"+inf.department_id+"'), base_wage=isnull(NULL,'"+inf.base_wage+"'), House_wage=isnull(NULL,'"+inf.House_wage+"') where id = "+inf.id,
     "update wage set late_days =isnull(NULL,'"+inf.late_days+"'), leave_days=isnull(NULL,'"+inf.leave_days+"'), year=isnull(NULL,'"+inf.year+"'), month=isnull(NULL,'"+inf.month+"'), ask_days=isnull(NULL,'"+inf.ask_days+"'), over_hours=isnull(NULL,'"+inf.over_hours+"') where id = "+inf.id,
     "update wage set base_wage =isnull(NULL,'"+inf.base_wage+"'), House_wage=isnull(NULL,'"+inf.House_wage+"'), should_wage=isnull(NULL,'"+inf.should_wage+"'), shouldnot_wage=isnull(NULL,'"+inf.shouldnot_wage+"'), real_wage=isnull(NULL,'"+inf.real_wage+"') where id = "+inf.id,
     "update rules set base_days =isnull(NULL,'"+inf.base_days+"'), late_wage=isnull(NULL,'"+inf.late_wage+"'), leave_wage=isnull(NULL,'"+inf.leave_wage+"'), ask_wage=isnull(NULL,'"+inf.ask_wage+"'), over_wage=isnull(NULL,'"+inf.over_wage+"') where id = "+inf.id
-    );
-  request.query(sql[inf.type-1], function(err, recordset){
+    ];
+  request.query(sql[inf.type-1], (err, recordset) => {
     if(err){
       res.send('error');
     }else{
@@ -142,12 +151,12 @@ app.post('/save', function(req, res){
   });
   
 });
-app.post('/add_new', function(req, res){
-  var name = req.body.name;
-  var superior_id = req.body.superior_id;
-  var department_id = req.body.department_id;
-  var request = new mssql.Request(con);
-  request.query("insert into base_information(name,superior_id,department_id) values('"+name+"','"+superior_id+"','"+department_id+"')", function(err, recordset){
+app.post('/add_new', (req, res) => {
+  let name = req.body.name;
+  let superior_id = req.body.superior_id;
+  let department_id = req.body.department_id;
+  let request = new mssql.Request(con);
+  request.query("insert into base_information(name,superior_id,department_id) values('"+name+"','"+superior_id+"','"+department_id+"')", (err, recordset) => {
     if(err){
       res.send('error');
     }else{
@@ -155,16 +164,16 @@ app.post('/add_new', function(req, res){
     }
   });
 });
-app.post('/add', function(req, res){
-  var id = req.body.id;
-  var name = req.body.name;
-  var department_id = req.body.department_id;
-  var request = new mssql.Request(con);
-  var base_wage,House_wage;
-  request.query("select base_wage,House_wage from base_information where id = "+id, function(err, recordset){
+app.post('/add', (req, res) => {
+  let id = req.body.id;
+  let name = req.body.name;
+  let department_id = req.body.department_id;
+  let request = new mssql.Request(con);
+  let base_wage,House_wage;
+  request.query("select base_wage,House_wage from base_information where id = "+id, (err, recordset) => {
       base_wage = recordset[0].base_wage;
       House_wage = recordset[0].House_wage;
-      request.query("insert into wage(e_id,name,d_id,base_wage,House_wage) values('"+id+"','"+name+"','"+department_id+"','"+base_wage+"','"+House_wage+"')", function(err, recordset){
+      request.query("insert into wage(e_id,name,d_id,base_wage,House_wage) values('"+id+"','"+name+"','"+department_id+"','"+base_wage+"','"+House_wage+"')", (err, recordset) => {
         if(err){
           res.send(err);
         }else{
@@ -175,11 +184,11 @@ app.post('/add', function(req, res){
  
 });
 
-app.post('/show_inf',function(req,res){
-  var request = new mssql.Request(con);
-  var i = req.body.type;
-  var sql = new Array("select * from base_information","select * from wage order by e_id,year,month", "select * from wage order by e_id,year,month","select * from rules");
-  request.query(sql[i-1], function(err, recordset){
+app.post('/show_inf',(req,res) =>{
+  let request = new mssql.Request(con);
+  let i = req.body.type;
+  let sql = ["select * from base_information","select * from wage order by e_id,year,month", "select * from wage order by e_id,year,month","select * from rules"];
+  request.query(sql[i-1], (err, recordset) => {
     if(err){
       res.send('error');
     }else{
